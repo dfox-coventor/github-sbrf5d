1f3962f6ddc83bfcba1d279067613da3f0f13dca
dfox-coventor <dfox@coventor.com>
2020-01-13 11:55:44 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// always put in slashes as the names of components to 
+// distinguish them from html components/modules(?) that have no hyphen
 @Component({
   selector: 'app-root',
   template: `
